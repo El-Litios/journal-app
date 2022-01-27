445017d25959d267e94aafecdd42bb6822a3c55e
El-Litios <carlosguzmanrojas00@gmail.com>
2022-01-26 23:54:29 -0300

    Layout de diario

---
+++ src/modules/daybook/layouts/DayBookLayout.vue
@@ -0,0 +1,24 @@
+<template>
+    <Navbar/>
+
+    <div class="d-flex">
+        <div class="col-">
+            <EntryList/>
+        </div>
+        <div class="col">
+            <router-view></router-view>
+        </div>
+    </div>
+
+</template>
+
+<script>
+import Navbar from '../components/Navbar.vue'
+import EntryList from '../components/EntryList.vue'
+
+export default {
+    components: {
+        Navbar, EntryList
+    }
+}
+</script>
