408affd3ba37d9aa57953c7ec074979300b3bf17
El-Litios <carlosguzmanrojas00@gmail.com>
2022-01-26 23:54:12 -0300

    Componente navbar

---
+++ src/modules/daybook/components/Navbar.vue
@@ -0,0 +1,14 @@
+<template>
+    <nav class="navbar bg-dark">
+        <a class="navbar-brand text-white">
+            <img src="@/assets/logo.png" alt="logo-vue" class="d-inline-block align-text-top mx-2" height="24">
+            Diario
+        </a>
+
+        <div class="d-flex">
+            <button class="btn btn-outline-info mx-2">
+                <i class="fas fa-sign-out-alt"></i>
+            </button>
+        </div>
+    </nav>
+</template>
