e8567cfe83aaf2201fcd221ff21f9586e37435c2
El-Litios <carlosguzmanrojas00@gmail.com>
2022-01-27 23:37:08 -0300

    creacion de componente entry

---
+++ src/modules/daybook/components/Entry.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="entry-container mb-3 pointer p-2">
+      <!-- Title -->
+      <div class="entry-title d-flex">
+          <span class="text-success fs-5 fw-bold">12</span>
+          <span class="mx-1 fs-5">Enero</span>
+          <span class="mx-1 fs-5">2022</span>
+      </div>
+      <div class="entry-description">
+          Descripcion de ejemplo
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+    .entry-container{
+        border-bottom: 1px solid #12e350;
+        transition: 0.2s all ease-in;
+
+        &:hover{
+            background-color: lighten($color: grey, $amount: 45);
+        }
+
+        .entry-description{
+            font-size: 12px;
+        }
+    }
+
+</style>
