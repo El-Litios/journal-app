a6ed2dafa6870b46096288702707a6c6a5c32a4a
El-Litios <carlosguzmanrojas00@gmail.com>
2022-01-28 23:54:26 -0300

    Agrega rutas hijas

--- src/modules/daybook/router/index.js
+++ src/modules/daybook/router/index.js
@@ -2,6 +2,15 @@ export default {
     name: 'daybook',
     component: () => import(/* webpackChunkName: 'daybook' */ '@/modules/daybook/layouts/DayBookLayout.vue'),
     children: [
-        
+        {
+            path: '',
+            name: 'No-entry',
+            component: () => import(/* webpackChunkName: 'daybook-no-entry' */ '@/modules/daybook/views/NoEntry.vue'),
+        },
+        {
+            path: ':id',
+            name: 'Entry',
+            component: () => import(/* webpackChunkName: 'daybook-entry' */ '@/modules/daybook/views/EntryView.vue'),
+        }
     ]
 }
