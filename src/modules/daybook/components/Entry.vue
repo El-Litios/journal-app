d5e827630143b12cc766b33dd2a43732fb546ce4
El-Litios <carlosguzmanrojas00@gmail.com>
2022-01-28 23:53:50 -0300

    Agrega redireccion a la ruta

--- src/modules/daybook/components/Entry.vue
+++ src/modules/daybook/components/Entry.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="entry-container mb-3 pointer p-2">
+  <div class="entry-container mb-3 pointer p-2" @click="$router.push({ name: 'Entry', params: { id: 1 } })">
       <!-- Title -->
       <div class="entry-title d-flex">
           <span class="text-success fs-5 fw-bold">12</span>
