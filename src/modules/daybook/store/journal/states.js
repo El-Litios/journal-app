b223f5b07cd0a151d89e7a5d293dab29c742e6b6
El-Litios <carlosguzmanrojas00@gmail.com>
2022-02-01 00:43:01 -0300

    Se cambia el id a string

--- src/modules/daybook/store/journal/states.js
+++ src/modules/daybook/store/journal/states.js
@@ -2,19 +2,19 @@ export default () => ({
     isLoading: true,
     entries: [
         {
-            id: new Date().getTime(),
+            id: '1',
             date: new Date().toDateString(),
             text: 'lorem ipsum ad nulla occeatuc eiusmon qui enim voluptate yustiu. Conquesat autute cillum laborum',
             picture: null,
         },
         {
-            id: new Date().getTime() + 10,
+            id: '2',
             date: new Date().toDateString(),
             text: 'lorem ipsum ad nulla occeatuc eiusmon qui enim voluptate yustiu. Conquesat autute cillum laborum ad coplor poresum',
             picture: null,
         },
         {
-            id: new Date().getTime() + 20,
+            id: '3',
             date: new Date().toDateString(),
             text: 'lorem ipsum ad nulla occeatuc eiusmon qui enim voluptate yustiu. Conquesat autute cillum laborum ad coplor  vaki olpe turruru',
             picture: null,
