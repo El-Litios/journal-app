46e9bad9acbc82105ed0db7e10803ce207833c53
El-Litios <carlosguzmanrojas00@gmail.com>
2022-02-02 22:45:32 -0300

    a

--- src/modules/daybook/store/journal/states.js
+++ src/modules/daybook/store/journal/states.js
@@ -1,23 +1,4 @@
 export default () => ({
     isLoading: true,
-    entries: [
-        {
-            id: '1',
-            date: new Date().toDateString(),
-            text: 'lorem ipsum ad nulla occeatuc eiusmon qui enim voluptate yustiu. Conquesat autute cillum laborum',
-            picture: null,
-        },
-        {
-            id: '2',
-            date: new Date().toDateString(),
-            text: 'lorem ipsum ad nulla occeatuc eiusmon qui enim voluptate yustiu. Conquesat autute cillum laborum ad coplor poresum',
-            picture: null,
-        },
-        {
-            id: '3',
-            date: new Date().toDateString(),
-            text: 'lorem ipsum ad nulla occeatuc eiusmon qui enim voluptate yustiu. Conquesat autute cillum laborum ad coplor  vaki olpe turruru',
-            picture: null,
-        }
-    ]
+    entries: []
 }) 
