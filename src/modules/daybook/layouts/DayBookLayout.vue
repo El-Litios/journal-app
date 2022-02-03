d3c4cdc5cd45a35d62d2a019d5318bb2179b64d8
El-Litios <carlosguzmanrojas00@gmail.com>
2022-02-02 22:43:54 -0300

    Agrega informacion de espera para cargar los datos

--- src/modules/daybook/layouts/DayBookLayout.vue
+++ src/modules/daybook/layouts/DayBookLayout.vue
@@ -1,7 +1,18 @@
 <template>
     <Navbar/>
 
-    <div class="d-flex">
+    <div v-if="isLoading" class="row justify-content-md-center">
+        <div class="col-3 alert-infor text-center mt-5">
+            Espere por favor...
+            <h3 class="mt-2">
+                <i class="fa fa-spin fa-sync">
+
+                </i>
+            </h3>
+        </div>
+    </div>
+
+    <div v-else class="d-flex">
         <div class="col-">
             <EntryList/>
         </div>
@@ -15,10 +26,23 @@
 <script>
 import Navbar from '../components/Navbar.vue'
 import EntryList from '../components/EntryList.vue'
+import { mapActions, mapState } from 'vuex'
 
 export default {
     components: {
         Navbar, EntryList
+    },
+
+    methods:{
+        ...mapActions('journal', ['laodEntries'])
+    },
+
+    computed: {
+        ...mapState('journal', ['isLoading'])
+    },
+
+    created() {
+        this.laodEntries()
     }
 }
 </script>
