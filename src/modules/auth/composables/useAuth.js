ad9616293818c646aaaffd8f89c007419a776512
El-Litios <carlosguzmanrojas00@gmail.com>
2023-05-21 15:48:18 -0400

    Composable auth

---
+++ src/modules/auth/composables/useAuth.js
@@ -0,0 +1,15 @@
+//import { useStore } from "vuex"
+
+const useAuth = () => {
+    //const store = useStore();
+
+    const createUser = async(user) => {
+        console.log(user);
+    }
+
+    return {
+        createUser
+    }
+}
+
+export default useAuth
