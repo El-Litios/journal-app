b12823bade2243711dda6a1537967536fa3df6fa
El-Litios <carlosguzmanrojas00@gmail.com>
2023-05-21 17:38:17 -0400

    Crear usuario

--- src/modules/auth/store/auth/actions.js
+++ src/modules/auth/store/auth/actions.js
@@ -1,3 +1,20 @@
-/* export const Actions =async ({commit}) => {
+import { auth } from "@/api/auth";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
-} */
+export const createUser = async ({ commit }, user) => {
+  const { email, password } = user;
+  try {
+    createUserWithEmailAndPassword(auth, email, password).then((res) => {
+      console.log(res);
+      commit();
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode);
+        console.log(errorMessage);
+      })
+  } catch (error) {
+    console.log('error aaaaa',error.reponse);
+  }
+};
