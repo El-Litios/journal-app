36dc23dcf038826adb215f46789e6f6eca87b195
El-Litios <carlosguzmanrojas00@gmail.com>
2023-05-26 00:58:50 -0400

    Mutation para states de usuario

--- src/modules/auth/store/auth/mutations.js
+++ src/modules/auth/store/auth/mutations.js
@@ -1,3 +1,10 @@
-/* export const Actions =  (state) => {
-    do mutations
-} */
+export const loginUser =  (state, user) => {
+    if(user){
+        localStorage.setItem('idToken', user.stsTokenManager.accessToken)
+        localStorage.setItem('refreshToken', user.stsTokenManager.refreshToken)
+        state.idToken = user.stsTokenManager.accessToken
+        state.refreshToken = user.stsTokenManager.refreshToken
+        state.user = user
+        state.status = 'authenticated'
+    }
+} 
