283a53013886743de89b4bab5ef59b13d7deedca
El-Litios <carlosguzmanrojas00@gmail.com>
2023-05-31 01:10:25 -0400

    Check del estado del token

--- src/modules/auth/composables/useAuth.js
+++ src/modules/auth/composables/useAuth.js
@@ -1,21 +1,30 @@
-import { useStore } from "vuex"
+import { computed } from "vue";
+import { useStore } from "vuex";
 
 const useAuth = () => {
-    const store = useStore();
+  const store = useStore();
 
-    const createUser = async(user) => {
-        const res = await store.dispatch('auth/createUser', user)
-        return res
-    }
+  const createUser = async (user) => {
+    const res = await store.dispatch("auth/createUser", user);
+    return res;
+  };
 
-    const signIn = async(user) => {
-        const res = await store.dispatch('auth/signInUser', user)
-        return res
-    }
+  const signIn = async (user) => {
+    const res = await store.dispatch("auth/signInUser", user);
+    return res;
+  };
 
-    return {
-        createUser,signIn
-    }
-}
+  const checkAuthStatus = async () => {
+    const res = await store.dispatch("auth/checkAuthenticationToken");
+    return res;
+  };
 
-export default useAuth
+  return {
+    createUser,
+    signIn,
+    checkAuthStatus,
+    authStatus: computed(() => store.getters["auth/currentUserState"]),
+  };
+};
+
+export default useAuth;
