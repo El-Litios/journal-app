f8789fdf7004481f9527a66c74708928ef78849b
El-Litios <carlosguzmanrojas00@gmail.com>
2023-05-31 00:07:43 -0400

    Agrega el codigo para el login de usuario

--- src/modules/auth/views/Login.vue
+++ src/modules/auth/views/Login.vue
@@ -1,8 +1,8 @@
 <template>
   <span class="login100-form-title p-b-41"> Ingresar </span>
-  <form class="login100-form validate-form p-b-33 p-t-5">
+  <form @submit.prevent="onSubmit" class="login100-form validate-form p-b-33 p-t-5">
     <div class="wrap-input100 validate-input" data-validate="Enter username">
-      <input class="input100" type="text" placeholder="Correo" required />
+      <input class="input100" v-model="userForm.email" type="text" placeholder="Correo" required />
       <span class="focus-input100" data-placeholder="&#xe82a;"></span>
     </div>
 
@@ -10,6 +10,7 @@
       <input
         class="input100"
         type="password"
+        v-model="userForm.password"
         placeholder="Contraseña"
         required
       />
@@ -27,5 +28,28 @@
 </template>
 
 <script>
-export default {};
+import { ref } from "vue";
+import useAuth from "../composables/useAuth";
+import Swal from 'sweetalert2'
+import {useRouter} from 'vue-router'
+export default {
+  setup(){
+    const router = useRouter();
+    const { signIn } = useAuth();
+    const userForm = ref({
+      email: "carlos@gmail.com",
+      password: "123456",
+    });
+
+    return {
+      userForm,
+      onSubmit: async () => {
+        const {ok, message} = await signIn(userForm.value)
+
+        if ( !ok) Swal.fire('Error', message, 'error')
+        else router.push( {name: 'No-entry'} )
+      },
+    };
+  }
+};
 </script>
