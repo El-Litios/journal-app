13c3da8b743ec2bf43b50b985733a38eb8088bb3
El-Litios <carlosguzmanrojas00@gmail.com>
2023-05-31 01:12:09 -0400

    Mutation para deslogear al usuario

--- src/modules/auth/store/auth/mutations.js
+++ src/modules/auth/store/auth/mutations.js
@@ -1,13 +1,22 @@
-export const loginUser =  (state, user) => {
-    if(user){
-        localStorage.setItem('idToken', user.stsTokenManager.accessToken)
-        localStorage.setItem('refreshToken', user.stsTokenManager.refreshToken)
-        state.idToken = user.stsTokenManager.accessToken
-        state.refreshToken = user.stsTokenManager.refreshToken
-        state.user = {
-            email: user.email,
-            name: user.displayName
-        }
-        state.status = 'authenticated'
-    }
-} 
+export const loginUser = (state, user) => {
+  if (user) {
+    localStorage.setItem("idToken", user.stsTokenManager.accessToken);
+    localStorage.setItem("refreshToken", user.stsTokenManager.refreshToken);
+    state.idToken = user.stsTokenManager.accessToken;
+    state.refreshToken = user.stsTokenManager.refreshToken;
+    state.user = {
+      email: user.email,
+      name: user.displayName,
+    };
+    state.status = "authenticated";
+  }
+};
+
+export const logoutUser = (state) => {
+  state.user = null;
+  state.refreshToken = null;
+  state.idToken = null;
+  state.status = "not-authenticated";
+  localStorage.removeItem("idToken");
+  localStorage.removeItem("refreshToken");
+};
