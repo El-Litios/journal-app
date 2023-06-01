410c4932587077036a38765d184eb69c45b1d9e0
El-Litios <carlosguzmanrojas00@gmail.com>
2023-06-01 01:48:01 -0400

    Interceptor para permitir el uso del backend por el token

--- src/api/journal.js
+++ src/api/journal.js
@@ -5,4 +5,13 @@ const journal = axios.create({
     baseURL: 'https://vue-journal-app-e6f46-default-rtdb.firebaseio.com'
 })
 
+journal.interceptors.request.use( (config => {
+    
+    config.params = {
+        auth: localStorage.getItem('idToken')
+    }
+    
+    return config
+}) )
+
 export default journal
